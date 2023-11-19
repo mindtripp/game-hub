4af0c735cfaa228f3ce330d6b23849f3517a481d
mindtripp <jaimealba1574@gmail.com>
2023-11-18 23:38:18 -0500

    added shadow

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  MindChildWidth: string;
 }
 
 interface FetchGames {
@@ -24,19 +25,25 @@ interface FetchGames {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     apiClient
       .get<FetchGames>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
     return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
